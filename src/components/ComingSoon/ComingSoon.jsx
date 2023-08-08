3440622e02d77952ddfd8d880e872105ff68721b
Mark Jerly Bundalian <mjbundalian61@gmail.com>
2023-08-08 13:58:18 +0800

    Add responsiveness to the coming soon

--- src/components/ComingSoon/ComingSoon.jsx
+++ src/components/ComingSoon/ComingSoon.jsx
@@ -11,14 +11,14 @@ export default function ComingSoon() {
       backgroundSize: 'cover',
     }}>
       {/* Background: https://wallpapersafari.com/w/LwoI4i */}
-      <div className="coming-soon-wrap">
-        <div className="w-1/3 mx-auto">
+      <div className="coming-soon-wrap px-5">
+        <div className="w-2/3 mx-auto md:w-1/3">
           <img src={rwtaLogoTrans} alt="RWTA logo" className="w-full" />
         </div>
         <div className="coming-soon-content text-center">
-          <h1 className="text-red-700 font-bold text-4xl mb-5">SOMETHING <span className="text-green-600">EXTRAORDINARY</span> IS IN THE WORKS!</h1>
+          <h1 className="text-red-700 font-bold mb-5 text-3xl md:text-4xl">SOMETHING <span className="text-green-600">EXTRAORDINARY</span> IS IN THE WORKS!</h1>
           <hr className="my-5" />
-          <p className="w-100 text-gray-800 text-lg md:w-8/12 md:mx-auto">We're hard at work constructing the future of forex education. Stay tuned for a transformative learning experience!</p>
+          <p className="w-100 text-gray-800 md:text-lg md:w-8/12 md:mx-auto">We're hard at work constructing the future of forex education. Stay tuned for a transformative learning experience!</p>
         </div>
         <div className="coming-soon-socmed flex flex-row justify-center items-center mt-10 gap-7">
           <a href="#" target="_blank" rel="noreferrer">
