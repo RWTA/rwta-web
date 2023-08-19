902240d156b129cc26260dc1e7467f6a72d42f11
Mark Jerly Bundalian <mjbundalian61@gmail.com>
2023-08-19 22:06:00 +0800

    Finish about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoins, faLightbulb } from '@fortawesome/free-solid-svg-icons'
-import placeholderImage from '../assets/rwta-trans.png'
+import { faChartLine, faLightbulb, faRocket } from '@fortawesome/free-solid-svg-icons'
 
 function About() {
   return (
@@ -20,30 +19,38 @@ function About() {
         </p>
       </div>
 
-      <div className="max-w-6xl mx-auto px-4 my-10">
-        <div className="flex justify-center items-center space-x-7">
-          <div className="box border-blue-200">
+      <div className="max-w-6xl mx-auto px-4 mt-10">
+        <div className="flex justify-center items-stretch space-x-7">
+          <div className="w-1/3 box border-blue-200 flex flex-col justify-between">
             <div className="w-20 mx-auto h-20 bg-blue-500 rounded-full flex items-center justify-center">
               <FontAwesomeIcon icon={faLightbulb} className="text-white text-4xl" />
             </div>
-            <h3 className="subheader-1 font-bold my-5 text-2xl">Right Mindset</h3>
-            <p className="par-1 text-lg">Forex isn't just about strategies, it's about mindset</p>
-          </div>
-          <div className="box border-blue-200">
-            <div className="w-20 mx-auto h-20 bg-blue-500 rounded-full flex items-center justify-center">
-              <FontAwesomeIcon icon={faCoins} className="text-white text-4xl" />
+            <div>
+              <h3 className="subheader-1 font-bold my-5 text-2xl">Learn The Ultimate Trading</h3>
+              <p className="par-1 text-lg">Discover how to trade successfully and make the most of opportunities.</p>
             </div>
-            <h3 className="subheader-1 font-bold my-5 text-2xl">Learn Forex</h3>
-            <p className="par-1 text-lg">Setup a brokerage account and learn forex</p>
           </div>
-          <div className="box border-blue-200">
+          <div className="w-1/3 box border-blue-200 flex flex-col justify-between">
             <div className="w-20 mx-auto h-20 bg-blue-500 rounded-full flex items-center justify-center">
-              <FontAwesomeIcon icon={faLightbulb} className="text-white text-4xl" />
+              <FontAwesomeIcon icon={faRocket} className="text-white text-4xl" />
             </div>
-            <h3 className="subheader-1 font-bold my-5 text-2xl">Right Mindset</h3>
-            <p className="par-1 text-lg">Forex isn't just about strategies, it's about mindset</p>
+            <div>
+              <h3 className="subheader-1 font-bold my-5 text-2xl">Start Your Trading Journey</h3>
+              <p className="par-1 text-lg">Join us to gain the skills and mindset for successful trading</p>
+            </div>
+          </div>
+          <div className="w-1/3 box border-blue-200 flex flex-col justify-between">
+            <div className="w-20 mx-auto h-20 bg-blue-500 rounded-full flex items-center justify-center">
+              <FontAwesomeIcon icon={faChartLine} className="text-white text-4xl" />
+            </div>
+            <div>
+              <h3 className="subheader-1 font-bold my-5 text-2xl">Master Trading, Boost Your Finances</h3>
+              <p className="par-1 text-lg">Learn practical trading strategies to shape your financial future</p>
+            </div>
           </div>
         </div>
+
+
       </div>
     </section>
   )
