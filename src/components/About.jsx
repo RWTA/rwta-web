74fb08f34bb573f83a0c9c190783375041cba20c
Mark Jerly Bundalian <mjbundalian61@gmail.com>
2023-10-26 00:47:44 +0800

    Add ids

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,21 +1,35 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faChartLine, faLightbulb, faRocket } from '@fortawesome/free-solid-svg-icons'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChartLine,
+  faLightbulb,
+  faRocket,
+} from "@fortawesome/free-solid-svg-icons";
 
 function About() {
   return (
-    <section className="bg-gray-100 py-16">
+    <section id="about" className="bg-gray-100 py-16">
       <div className="max-w-6xl mx-auto px-4">
-        <h3 className="header-1 font-bold mb-3 text-4xl text-center">YOUR PATH TO FINANCIAL EMPOWERMENT BEGINS <span className="text-green-500">NOW!</span></h3>
+        <h3 className="header-1 font-bold mb-3 text-4xl text-center">
+          YOUR PATH TO FINANCIAL EMPOWERMENT BEGINS{" "}
+          <span className="text-green-500">NOW!</span>
+        </h3>
         <div className="divider-1"></div>
         <p className="par-1 w-5/6 mx-auto text-lg mb-4 text-center">
-          Dream of financial freedom? Join us on an enlightening voyage to the world of forex trading. Imagine turning currency exchanges into your own source of wealth. Our Forex Academy unveils this potential.
+          Dream of financial freedom? Join us on an enlightening voyage to the
+          world of forex trading. Imagine turning currency exchanges into your
+          own source of wealth. Our Forex Academy unveils this potential.
         </p>
         <p className="par-1 w-5/6 mx-auto text-lg mb-4 text-center">
-          But success in forex isn't just about strategies - it's about the right mindset. Our academy not only equips you with expert guidance, strategies, and risk management skills but also corrects and strengthens the psychology behind forex trading. Discover how millions are made daily in forex trading.
+          But success in forex isn't just about strategies - it's about the
+          right mindset. Our academy not only equips you with expert guidance,
+          strategies, and risk management skills but also corrects and
+          strengthens the psychology behind forex trading. Discover how millions
+          are made daily in forex trading.
         </p>
         <p className="par-1 w-5/6 mx-auto text-lg text-center">
-          Empower yourself. Enroll today and chart a course toward financial independence with us.
+          Empower yourself. Enroll today and chart a course toward financial
+          independence with us.
         </p>
       </div>
 
@@ -23,37 +37,58 @@ function About() {
         <div className="flex justify-center items-stretch space-x-7">
           <div className="w-1/3 box border-blue-200 flex flex-col justify-between">
             <div className="w-20 mx-auto h-20 bg-blue-500 rounded-full flex items-center justify-center">
-              <FontAwesomeIcon icon={faLightbulb} className="text-white text-4xl" />
+              <FontAwesomeIcon
+                icon={faLightbulb}
+                className="text-white text-4xl"
+              />
             </div>
             <div>
-              <h3 className="subheader-1 font-bold my-5 text-2xl">Learn The Ultimate Trading</h3>
-              <p className="par-1 text-lg">Discover how to trade successfully and make the most of opportunities.</p>
+              <h3 className="subheader-1 font-bold my-5 text-2xl">
+                Learn The Ultimate Trading
+              </h3>
+              <p className="par-1 text-lg">
+                Discover how to trade successfully and make the most of
+                opportunities.
+              </p>
             </div>
           </div>
           <div className="w-1/3 box border-blue-200 flex flex-col justify-between">
             <div className="w-20 mx-auto h-20 bg-blue-500 rounded-full flex items-center justify-center">
-              <FontAwesomeIcon icon={faRocket} className="text-white text-4xl" />
+              <FontAwesomeIcon
+                icon={faRocket}
+                className="text-white text-4xl"
+              />
             </div>
             <div>
-              <h3 className="subheader-1 font-bold my-5 text-2xl">Start Your Trading Journey</h3>
-              <p className="par-1 text-lg">Join us to gain the skills and mindset for successful trading</p>
+              <h3 className="subheader-1 font-bold my-5 text-2xl">
+                Start Your Trading Journey
+              </h3>
+              <p className="par-1 text-lg">
+                Join us to gain the skills and mindset for successful trading
+              </p>
             </div>
           </div>
           <div className="w-1/3 box border-blue-200 flex flex-col justify-between">
             <div className="w-20 mx-auto h-20 bg-blue-500 rounded-full flex items-center justify-center">
-              <FontAwesomeIcon icon={faChartLine} className="text-white text-4xl" />
+              <FontAwesomeIcon
+                icon={faChartLine}
+                className="text-white text-4xl"
+              />
             </div>
             <div>
-              <h3 className="subheader-1 font-bold my-5 text-2xl">Master Trading, Boost Your Finances</h3>
-              <p className="par-1 text-lg">Learn practical trading strategies to shape your financial future</p>
+              <h3 className="subheader-1 font-bold my-5 text-2xl">
+                Master Trading, Boost Your Finances
+              </h3>
+              <p className="par-1 text-lg">
+                Learn practical trading strategies to shape your financial
+                future
+              </p>
             </div>
           </div>
         </div>
-
-
       </div>
     </section>
-  )
+  );
 }
 
-export default About
+export default About;
--- src/components/FinancialFreedom.jsx
+++ src/components/FinancialFreedom.jsx
@@ -1,10 +1,11 @@
-import { faCheck } from '@fortawesome/free-solid-svg-icons';
-import financialFreedom from '../assets/financial-freedom.jpg';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import financialFreedom from "../assets/financial-freedom.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function FinancialFreedom() {
   return (
     <section
+      id="freedom"
       className="bg-cover bg-fixed bg-center relative z-0"
       style={{ backgroundImage: `url(${financialFreedom})` }}
     >
@@ -13,25 +14,48 @@ function FinancialFreedom() {
       <div className="relative z-20 py-16">
         <div className="max-w-6xl mx-auto px-4">
           <div id="ff-content">
-            <h3 className="header-2 font-bold mb-3 text-4xl text-center">UNLOCK YOUR WAY TO FINANCIAL FREEDOM</h3>
+            <h3 className="header-2 font-bold mb-3 text-4xl text-center">
+              UNLOCK YOUR WAY TO FINANCIAL FREEDOM
+            </h3>
             <div className="divider-2"></div>
             <p className="par-2 w-5/6 mx-auto text-lg mb-4 text-center">
-              Your journey towards success and financial freedom begins with the decision to take proactive steps towards realizing your dreams. Seize control of your financial destiny by enrolling in the Financial Freedom Project Program, where you'll learn the essential tools and strategies needed to create lasting wealth.
+              Your journey towards success and financial freedom begins with the
+              decision to take proactive steps towards realizing your dreams.
+              Seize control of your financial destiny by enrolling in the
+              Financial Freedom Project Program, where you'll learn the
+              essential tools and strategies needed to create lasting wealth.
             </p>
             <p className="par-2 w-5/6 mx-auto text-lg text-center">
-              Embrace the opportunity to gain valuable insights and knowledge that will equip you to navigate the complexities of wealth creation with confidence. By enrolling in our program, you're taking a significant stride towards achieving financial freedom and setting the stage for a life of abundance and fulfillment.
+              Embrace the opportunity to gain valuable insights and knowledge
+              that will equip you to navigate the complexities of wealth
+              creation with confidence. By enrolling in our program, you're
+              taking a significant stride towards achieving financial freedom
+              and setting the stage for a life of abundance and fulfillment.
             </p>
           </div>
 
           <div className="flex justify-center items-start space-x-12 mt-10">
             <div id="yt-vid-1" className="w-1/2">
-              <iframe className="w-full" height="315" src="https://www.youtube.com/embed/LfhOrP0u6f8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+              <iframe
+                className="w-full"
+                height="315"
+                src="https://www.youtube.com/embed/LfhOrP0u6f8"
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowfullscreen
+              ></iframe>
             </div>
             <div id="yt-vid-1" className="w-1/2">
-              <h3 className="subheader-2 font-bold my-5 text-2xl">How To Start Trading Forex</h3>
+              <h3 className="subheader-2 font-bold my-5 text-2xl">
+                How To Start Trading Forex
+              </h3>
               <div className="flex justify-start space-x-5 items-center mb-3">
                 <div className="w-10 h-10 border-2 border-green-700 rounded-full flex items-center justify-center">
-                  <FontAwesomeIcon icon={faCheck} className="text-green-700 text-xl" />
+                  <FontAwesomeIcon
+                    icon={faCheck}
+                    className="text-green-700 text-xl"
+                  />
                 </div>
                 <div>
                   <p className="par-2 text-lg">Learn about forex and trading</p>
@@ -39,7 +63,10 @@ function FinancialFreedom() {
               </div>
               <div className="flex justify-start space-x-5 items-center mb-3">
                 <div className="w-10 h-10 border-2 border-green-700 rounded-full flex items-center justify-center">
-                  <FontAwesomeIcon icon={faCheck} className="text-green-700 text-xl" />
+                  <FontAwesomeIcon
+                    icon={faCheck}
+                    className="text-green-700 text-xl"
+                  />
                 </div>
                 <div>
                   <p className="par-2 text-lg">Setup brokerage account</p>
@@ -47,21 +74,24 @@ function FinancialFreedom() {
               </div>
               <div className="flex justify-start space-x-5 items-center mb-3">
                 <div className="w-10 h-10 border-2 border-green-700 rounded-full flex items-center justify-center">
-                  <FontAwesomeIcon icon={faCheck} className="text-green-700 text-xl" />
+                  <FontAwesomeIcon
+                    icon={faCheck}
+                    className="text-green-700 text-xl"
+                  />
                 </div>
                 <div>
-                  <p className="par-2 text-lg">Invest time and effort on studying forex</p>
+                  <p className="par-2 text-lg">
+                    Invest time and effort on studying forex
+                  </p>
                 </div>
               </div>
-              <button className="btn-2 mt-4">
-                ENROLL NOW!
-              </button>
+              <button className="btn-2 mt-4">ENROLL NOW!</button>
             </div>
           </div>
         </div>
       </div>
     </section>
-  )
+  );
 }
 
-export default FinancialFreedom
+export default FinancialFreedom;
